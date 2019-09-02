8901d825da9869b1bf15c31262c1f08d311b35c4
Vikrant Chudhry <v.chudhry@V.Chudhry>
2019-09-02 17:16:12 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter : this.props.clicks
+    };
+
+    this.changevalue = this.changevalue.bind(this);
+}
+  
+ changevalue = function(event,props) {
+   event.preventDefault();
+    this.setState({counter : this.state.counter+1});
+    console.log(this.state.counter);
+    }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button type = "button"  onClick={this.changevalue} value={this.state.counter}></button>
+    <div>{this.state.counter}</div>
+    
+      this div has been clicked <h1></h1>
     </div>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let counter =1;
+ReactDOM.render(<App clicks={0}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
